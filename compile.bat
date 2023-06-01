512ae5cab2590d54ad9254eb7a03369e6a7092c6
ClioCJS <ClioCJS@gmail.com>
2023-06-01 17:34:14 -0400

    BAT file typos on 2023/06/01 -- 5:34:05 PM -- Thursday

--- compile.bat
+++ compile.bat
@@ -3,7 +3,7 @@
 rem CONFIGURATION: Define project/script.py name:
     set BASENAME=ingest_ics
 
-rem SETUP: Check parameters, set variables, and update our local code to our GIT repository
+rem SETUP: Check parameters, set variables, and update our local code to our local GIT repository
     set MODE=%1
     set OUR_BUILD_DIR=dist\%BASENAME%
     set CREATED_EXE_FILE=%OUR_BUILD_DIR%\%BASENAME%.exe
@@ -17,17 +17,18 @@ rem BRANCHING: Skip the build if we say to
     )
 
 rem ADVICE: Let us know about some options we may forget about
-    call advice if we lose our spec file the timezone bug can likely be fixed by adding --collect-data dateutil to our build optoins here
-    call advice rem ember: --onefile, --onedir
+    call advice if we lose our spec file the timezone bug can likely be fixed by adding --collect-data dateutil to our build options here
+    call advice remember: --onefile, --onedir
 
 rem "MAKE CLEAN": Clean out files from previous builds
     :Make
-    call validate-in-path py2exe.bat clean.bat
+    call validate-in-path clean.bat
     call clean.bat
 
 rem "MAKE MAKE": Compile the EXE file ***************************************************************************************
     pushd
     REM optionally add --onedir --onefile here 
+    call validate-in-path py2exe.bat
     call py2exe.bat %BASENAME%.spec  %*
     popd
 
