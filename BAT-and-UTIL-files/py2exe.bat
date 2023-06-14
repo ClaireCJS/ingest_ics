01dc1cbb32663b23100337b507cd0abe0ddd32ad
ClioCJS <ClioCJS@gmail.com>
2023-06-14 11:56:34 -0400

    Wed 11:56AM: Megadeth - Peace Sells

---
+++ BAT-and-UTIL-files/py2exe.bat
@@ -0,0 +1,21 @@
+@Echo off
+
+REM py2exe.bat is now a catch-all for the intent of EXE compling, which branches in
+REM such a way that if we don't remember how to do things, they should still work
+
+set FILE=%@UNQUOTE[%1]
+set  EXT=%@EXT[%FILE]
+
+call validate-environment-variable FILE
+
+if "%EXT%" eq "spec" (
+     REM call debug call "py.spec2exe.bat  %FILE%"
+                    call  py.spec2exe.bat "%FILE%"
+)
+
+if "%EXT%" eq  "py"  (
+    set SPEC=%@NAME[%FILE].spec
+    if not exist "%SPEC%" call py2spec.bat     "%FILE%"
+    if     exist "%SPEC%" call py.spec2exe.bat "%SPEC%"buil 
+
+)
